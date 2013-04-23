2fc76ab1bfbad88b1b8ce3b6a47b1626aecf7fa5
Tim Johnston <tim.defrag@gmail.com>
2013-04-22 20:45:07 -0500

    Define example difficulties config

--- app/js/app/config.js
+++ app/js/app/config.js
@@ -1,8 +1,19 @@
-
-
-
 (function(exports) {
+
   exports.config = {
-  	
+
+    // Difficulty Levels
+    diffs: [
+      { label: 'Easy',    speedCoef: 0.8,  scoreCoef: 0.8 },
+      { label: 'Medium',  speedCoef: 1.0,  scoreCoef: 1.0 },
+      { label: 'Hard',    speedCoef: 1.5,  scoreCoef: 1.5 }
+    ],
+
+    // Arena
+    field: {
+      width: 400,
+      height: 400
+    }
   };
+
 })(window);
--- app/js/app/main.js
+++ app/js/app/main.js
@@ -3,7 +3,4 @@
 (function(exports) {
 
 
-
-  
-
 })(window);
